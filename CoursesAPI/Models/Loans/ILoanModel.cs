1e84e7b86191b302a8e8f298ea7d57d931886c73
Heidarie <dawidogly@gmail.com>
2022-06-08 21:43:46 +0200

    Loan models

--- CoursesAPI/Models/Loans/ILoanModel.cs
+++ CoursesAPI/Models/Loans/ILoanModel.cs
@@ -5,8 +5,7 @@
         public User UserId { get; set; }
         public DateTime LoanFrom { get; set; }
         public DateTime LoanTo { get; set; }
-        public int LoanDaysSummary { get; set; }
         public Car Car { get; set; }
-        public int CalculateLoaningDays(DateTime LoanFrom, DateTime LoanTo);
+        
     }
 }
---
+++ CoursesAPI/Models/Loans/LoanModel.cs
@@ -0,0 +1,26 @@
+﻿namespace CoursesAPI.Models.Loans
+{
+    public class LoanModel : ILoanModel
+    {
+        public LoanModel(User userId, DateTime loanFrom, DateTime loanTo, Car car)
+        {
+            UserId = userId;
+            LoanFrom = loanFrom;
+            LoanTo = loanTo;
+            LoanDaysSummary = CalculateLoaningDays(loanFrom,loanTo);
+            Car = car;
+        }
+
+        public User UserId { get; set; }
+        public DateTime LoanFrom { get; set; }
+        public DateTime LoanTo { get; set; }
+        private int LoanDaysSummary { get; set; }
+        public Car Car { get ; set ; }
+
+        private int CalculateLoaningDays(DateTime LoanFrom, DateTime LoanTo)
+        {
+            var days = LoanTo - LoanFrom;
+            return days.Days;
+        }
+    }
+}
