567ff1826c754ca3704c3da0c76d1e91645b021c
Wojciech Nowicki <95188715+wkn12@users.noreply.github.com>
2022-06-08 19:35:30 +0200

    modal + package

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,32 +4,32 @@ import car from "./../../car.png"
 const Home = () => {
     return (
         <div className="Home">
-        <img src={BG} width="100%"></img>
-        <div className="homeBoxes row">
-            <div className="homeBox  col-md-4 ">
-                <p className="homeBoxTitle">Wypożyczaj auta</p>
-                <div className="homeBoxContent">Przeglądaj, wybierz, zapłać. Dzięki nam wypożyczanie aut jest dziecinnie proste.
-                Skorzystaj z naszej strony, by móc cieszyć się jazdą twoim wymarzonym samochoed lub znaleźć auto zastępczę jeśli twój pojazd uległ awarii.</div>
-                <p className="homeBoxLink"><a>Przeglądaj➔</a></p>
-            </div>
-            <div className="homeBox col-md-4">
-                <p className="homeBoxTitle">Zarejestruj się</p>
-                <div className="homeBoxContent">Aby w pełnii korzystać z naszych usług zarejestruj się. Dzięki rejestracji szybciej i łatwiej będziesz mógł
-                wynajmować auta, oraz korzystać z promocji.</div>
-                <p className="homeBoxLink"><a>Stwórz konto➔</a></p>
-            </div>
-            <div className="homeBox  col-md-4">
-                <p className="homeBoxTitle">title</p>
-                <div className="homeBoxContent">content</div>
-                <p className="homeBoxLink">➔</p>
-            </div>
+            <img src={BG} width="100%"></img>
+            <div className="homeBoxes row">
+                <div className="homeBox  col-md-4 ">
+                    <p className="homeBoxTitle">Wypożyczaj auta</p>
+                    <div className="homeBoxContent">Przeglądaj, wybierz, zapłać. Dzięki nam wypożyczanie aut jest dziecinnie proste.
+                        Skorzystaj z naszej strony, by móc cieszyć się jazdą twoim wymarzonym samochoed lub znaleźć auto zastępczę jeśli twój pojazd uległ awarii.</div>
+                    <p className="homeBoxLink"><a>Przeglądaj➔</a></p>
+                </div>
+                <div className="homeBox col-md-4">
+                    <p className="homeBoxTitle">Zarejestruj się</p>
+                    <div className="homeBoxContent">Aby w pełnii korzystać z naszych usług zarejestruj się. Dzięki rejestracji szybciej i łatwiej będziesz mógł
+                        wynajmować auta, oraz korzystać z promocji.</div>
+                    <p className="homeBoxLink"><a>Stwórz konto➔</a></p>
+                </div>
+                <div className="homeBox  col-md-4">
+                    <p className="homeBoxTitle">Kursy doszkalające</p>
+                    <div className="homeBoxContent">content</div>
+                    <p className="homeBoxLink">➔</p>
+                </div>
             </div>
             <div className="row">
-                   <img className="photoLeft col-md-4" style={{padding:"0",marginTop:"2%"}} src={car}/>
-              
-                <div className="textRight col-md-6" style={{marginTop:"2%"}}>
-                <p className="homeBoxTitle">title</p>
-                <div className="homeBoxContent">content</div>
+                <img className="photoLeft col-md-4" style={{ padding: "0", marginTop: "2%" }} src={car} />
+
+                <div className="textRight col-md-6" style={{ marginTop: "2%" }}>
+                    <p className="homeBoxTitle">title</p>
+                    <div className="homeBoxContent">content</div>
                 </div>
             </div>
         </div>
--- src/components/rent/rent.js
+++ src/components/rent/rent.js
@@ -5,11 +5,12 @@ import { Col } from "react-bootstrap";
 import { } from "../../api/Api";
 import Cars from "../../jsonTestData/cars.json"
 import car from "./../../car.png"
+import Modal from "react-modal";
 class Rent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+                isOpen:false
         }
     }
 
@@ -17,6 +18,11 @@ class Rent extends Component {
         this.setState({
             ...this.state,
             [e.target.id]: e.target.value
+        })
+    }
+    toggleModal = () =>{
+        this.setState({
+            isOpen:!this.state.isOpen
         })
     }
 
@@ -85,7 +91,7 @@ class Rent extends Component {
                                     {<h2 className="price">{x.PricePerDay} zł</h2>
                                     }
 
-                                    <button className="mt-auto btn btn-lg btn-block btn-outline-primary" > Wynajmij</button>
+                                    <button className="mt-auto btn btn-lg btn-block btn-outline-primary" onClick={()=>{this.toggleModal()}}> Wynajmij</button>
 
 
                                 </div>
@@ -96,6 +102,42 @@ class Rent extends Component {
                     })}
 
                 </div>
+                <Modal
+                    isOpen={this.state.isOpen}
+                    onRequestClose={this.toggleModal}
+                    style={{
+                        overlay: {
+                            position: 'fixed',
+                            zIndex: 1020,
+                            top: 0,
+                            left: 0,
+                            width: '100vw',
+                            height: '100vh',
+                            background: 'rgba(255, 255, 255, 0.75)',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        },
+                        content: {
+                            background: 'white',
+                            width: '45rem',
+                            maxWidth: 'calc(100vw - 10rem)',
+                            minHeight: '300px',
+                            maxHeight: 'calc(100vh - 2rem)',
+                            overflowY: 'auto',
+                            position: 'relative',
+                            border: '1px solid #ccc',
+                            borderRadius: '0.3rem',
+                        }
+                    }}
+                    contentLabel="My dialog"
+                >
+                    <div>
+                        
+                    <button className="mt-auto btn btn-lg btn-block btn-outline-danger" onClick={()=>{this.toggleModal()}}> Anuluj</button>
+                       
+                    </div>
+                </Modal>
             </div>
         );
     }
