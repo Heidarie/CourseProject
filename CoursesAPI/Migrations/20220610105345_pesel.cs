2fbebd3f3f4b7e45fcff2e60c2cca6ec974b7bea
Heidarie <dawidogly@gmail.com>
2022-06-10 12:54:22 +0200

    pesel as bigint on sql

---
+++ CoursesAPI/Migrations/20220610105345_pesel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoursesAPI.Migrations
+{
+    public partial class pesel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "PeselNumber",
+                table: "AspNetUsers",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PeselNumber",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
--- CoursesAPI/Models/User.cs
+++ CoursesAPI/Models/User.cs
@@ -9,7 +9,7 @@ namespace CoursesAPI.Models
         public string GivenName { get; set; }
         [Column(TypeName = "nvarchar(50)")]
         public string FamilyName { get; set; }
-        [Column(TypeName = "integer")]
+        [Column(TypeName = "bigint")]
         public long PeselNumber { get; set; }
         public string? RefreshToken { get; set; }
         public DateTime RefreshTokenExpiryTime { get; set; }
