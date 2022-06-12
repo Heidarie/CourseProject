1d349df90612dfdf1404a1a95e0ea3192816de0a
Heidarie <dawidogly@gmail.com>
2022-06-12 10:22:13 +0200

    Summary price

--- CoursesAPI/Models/Users/UserResevationModel.cs
+++ CoursesAPI/Models/Users/UserResevationModel.cs
@@ -9,10 +9,12 @@ namespace CoursesAPI.Models.Users
             Car = new CarModel(loan.Car);
             From = loan.LoanFrom;
             To = loan.LoanTo;
+            SummaryPrice = (loan.Car.PricePerDay * loan.LoanDaysSummary);
         }
         public CarModel Car { get; set; }
         public DateTime From { get; set; }
         public DateTime To { get; set; }
+        public float SummaryPrice { get; set; }
 
     }
 }
