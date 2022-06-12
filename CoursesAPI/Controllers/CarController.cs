bd9b66759a57a2e61ae48f512c2b9cd82f077365
Heidarie <dawidogly@gmail.com>
2022-06-12 19:34:30 +0200

    regex video id

--- CoursesAPI/Controllers/CarController.cs
+++ CoursesAPI/Controllers/CarController.cs
@@ -3,6 +3,7 @@ using CoursesAPI.Models.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace CoursesAPI.Controllers
 {
@@ -28,6 +29,7 @@ namespace CoursesAPI.Controllers
         [Route("create-car")]
         public IActionResult CreateCar([FromForm] CarModel car)
         {
+            car.YTMovie = RegexYtUrl(car.YTMovie);
             var result = DatabaseManager.CreateCar(car);
             if(result)
                 return Ok();
@@ -39,6 +41,14 @@ namespace CoursesAPI.Controllers
         public IEnumerable<CarModel> FilterCars([FromBody] FilterModel filter)
         {
             return DatabaseManager.FilterCars(filter);
+        }
+
+        private string RegexYtUrl(string ytUrl)
+        {
+            string pattern = @"^(?:https?\:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v\=))([\w-]{10,12})(?:$|\&|\?\#).*";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(ytUrl);
+            return match.ToString();
         }
     }
 }
